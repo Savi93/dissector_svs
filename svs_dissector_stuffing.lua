32aa17ce222abbf67979f6baf880d3ebb9c54cac
David Savev <david.savev@ivecogroup.com>
2024-07-12 16:06:03 +0200

    Tracking di versionamento versione 1.0, merge manuale effettuato su versione 0.0

--- svs_dissector_stuffing.lua
+++ svs_dissector_stuffing.lua
@@ -2,12 +2,13 @@ ESCAPE_CHAR = 0xAA
 FLAG_CHAR = 0x7E
 svs_protocol = Proto("SVS_PROTOCOL_STUFFING",  "SVS_PROTOCOL_STUFFING")
 
-flagstart_type =ProtoField.uint8("SVS_PROTOCOL.FLAGSTART","FlagStart",base.HEX)
+flagstart_type = ProtoField.uint8("SVS_PROTOCOL.FLAGSTART","FlagStart",base.HEX)
 id_type = ProtoField.uint8("SVS_PROTOCOL.ID","ID",base.HEX)
-counter_type =ProtoField.uint8("SVS_PROTOCOL.Counter","Counter",base.DEC)
-length_type =ProtoField.uint8("SVS_PROTOCOL.Length","Length",base.DEC)
-typecommand_type =ProtoField.uint8("SVS_PROTOCOL.TypeCommand","TypeCommand",base.HEX)
-selectedcamera_type =ProtoField.uint16("SVS_PROTOCOL.SelectedCamera","SelectedCamera",base.BIN)
+counter_type = ProtoField.uint8("SVS_PROTOCOL.Counter","Counter",base.DEC)
+length_type = ProtoField.uint8("SVS_PROTOCOL.Length","Length",base.DEC)
+typecommand_type = ProtoField.uint8("SVS_PROTOCOL.TypeCommand","TypeCommand",base.HEX)
+selectedcamera_type = ProtoField.uint16("SVS_PROTOCOL.SelectedCamera","SelectedCamera",base.BIN)
+camview_type = ProtoField.uint32("SVS_PROTOCOL.CamView","Camera",base.HEX)
 parameters_type = ProtoField.uint8("SVS_PROTOCOL.Parameters","Parameters",base.HEX)
 camera_capabilities = ProtoField.uint8("SVS_PROTOCOL.Capabilities","Camera Capabilities",base.HEX)
 camera_status = ProtoField.uint8("SVS_PROTOCOL.Status","Camera Status",base.HEX)
@@ -19,7 +20,7 @@ reserved_type = ProtoField.uint16("SVS_PROTOCOL.Reserved","Reserved",base.HEX)
 checksum_type = ProtoField.uint8("SVS_PROTOCOL.Checksum","Checksum",base.HEX)
 flagstop_type =ProtoField.uint8("SVS_PROTOCOL.FLAGSTOP","FlagStop",base.HEX)
 
-svs_protocol.fields = {flagstart_type, id_type,counter_type, length_type, typecommand_type, selectedcamera_type, parameters_type, camera_capabilities, camera_status, camera_monitor, camera_ROI, camera_Polarity, extended_type, reserved_type, checksum_type, flagstop_type}
+svs_protocol.fields = {flagstart_type, id_type,counter_type, length_type, typecommand_type, selectedcamera_type, parameters_type, camview_type, camera_capabilities, camera_status, camera_monitor, camera_ROI, camera_Polarity, extended_type, reserved_type, checksum_type, flagstop_type}
 
 function removeEscapeChars(buffer_copy)
         local empty = ByteArray.new()
@@ -44,6 +45,18 @@ function removeEscapeChars(buffer_copy)
         return ByteArray.tvb(empty, "RETURN_TVB")
 end
 
+function computeSelectedCameraString(sel_camera_buff)
+	sel_camera_str = ""
+
+	for i = 0, 14, 1 do
+		if bit.band(bit.rshift(sel_camera_buff, i), 0x01) == 0x01 then
+			sel_camera_str = sel_camera_str .. "Camera " .. i .. ", "
+		end
+	end
+
+	return sel_camera_str
+end
+
 function svs_protocol.dissector(buffer, pinfo, tree)
 	pinfo.cols.protocol = svs_protocol.name;
 
@@ -51,11 +64,11 @@ function svs_protocol.dissector(buffer, pinfo, tree)
 	subtree = tree:add(svs_protocol, buffer())
 
 	type = buffer(1, 1):uint()
-	if type == 0x35 then  -- if the packet is of type commands
+	if type == 0x35 then  --if the packet is of type commands
 		mtype_str = "COMMANDS"
 		command = buffer(4,1):uint() 
 
-	elseif type == 0xCA then -- if the packet is of type status
+	elseif type == 0xCA then --if the packet is of type status
 		mtype_str = "STATUS"
 		command = buffer(6,1):uint() 
 	end
@@ -104,30 +117,54 @@ function svs_protocol.dissector(buffer, pinfo, tree)
 	subtree:add(counter_type,buffer(2,1)):append_text("")
 	subtree:add(length_type,buffer(3,1)):append_text("")
 
-	if mtype_str == "COMMANDS" then -- if the packet is of type commands
+	if mtype_str == "COMMANDS" then --if the packet is of type commands
 		subtree:add(typecommand_type,buffer(4,1)):append_text(" (" .. mycmd_str .. ")")
-		subtree:add(selectedcamera_type,buffer(5,2)):append_text("")
+		sel_camera_buff = buffer(5,2):uint()
+		sel_camera_str = computeSelectedCameraString(sel_camera_buff)
+		subtree:add(selectedcamera_type, buffer(5,2)):append_text(" (" .. sel_camera_str .. ") ")
 		subtree:add(parameters_type,buffer(7,1)):append_text(" (" .. params_str .. ")")
 		subtree:add(reserved_type,buffer(8,2)):append_text("")
 		subtree:add(checksum_type,buffer(10,1)):append_text("")
 		subtree:add(flagstop_type,buffer(11,1)):append_text("")
 
-	elseif mtype_str == "STATUS" then -- if the packet is of type status
-		subtree:add(selectedcamera_type, buffer(4,2)):append_text("")
+	elseif mtype_str == "STATUS" then --if the packet is of type status
+		sel_camera_buff = buffer(4,2):uint()
+		sel_camera_str = computeSelectedCameraString(sel_camera_buff)
+		subtree:add(selectedcamera_type, buffer(4,2)):append_text(" (".. sel_camera_str .. ") ")
 		subtree:add(typecommand_type, buffer(6,1)):append_text(" (" .. mycmd_str .. ")")
 		subtree:add(parameters_type, buffer(7,1)):append_text(" (" .. params_str .. ")")
 
 		index = 0
 		for i = 10, 63, 4 do
-			minitree = subtree:add("CAMERA" .. index, buffer())
+			minitree = subtree:add(camview_type, buffer(i-2, 4)):append_text(" (NR. " .. index .. ")")
 
-			capab = buffer(i-2,2)
-			first = buffer(i,1):uint()
-			second = buffer(i+1,1):uint()
+			capab = buffer(i-2,2):uint()
+			capab_str = ""
+			cam_stat_monitor = buffer(i,1):uint()
+			cam_roi_polarity = buffer(i+1,1):uint()
 
-			minitree:add(camera_capabilities, capab):append_text("")
+			if(bit.band(capab, 0x0001) == 0x0001) then
+				capab_str = capab_str .. "Camera con sensore diurno, "
+			end
+			if(bit.band(capab, 0x0002) == 0x0002) then
+				capab_str = capab_str ..  "Camera con sensore IR, "
+			end
+			if(bit.band(capab, 0x0400) == 0x0400) then
+				capab_str = capab_str .. "Camera con heater, "
+			end
+			if(bit.band(capab, 0x0800) == 0x0800) then
+				capab_str = capab_str .. "Camera con washer, "
+			end
+			if(bit.band(capab, 0x1000) == 0x1000) then
+				capab_str = capab_str .. "Camera con CAN, "
+			end
+			if(bit.band(capab, 0x2000) == 0x2000) then
+				capab_str = capab_str .. "Camera con RS232, "
+			end
 
-			cam_stat_filtered = bit.band(first, 0x0F)
+			minitree:add(camera_capabilities, buffer(i-2,2)):append_text(" (" .. capab_str .. ")")
+
+			cam_stat_filtered = bit.band(cam_stat_monitor, 0x0F)
 			if cam_stat_filtered == 0x00 then
 				cam_stat_str = "Stato della telecamera non disponibile"
 			elseif cam_stat_filtered == 0x01 then
@@ -138,7 +175,7 @@ function svs_protocol.dissector(buffer, pinfo, tree)
 				cam_stat_str = "Errore nella rilevazione del segnale video"
 			end
 
-			cam_monitor_filtered = bit.rshift(first, 4)
+			cam_monitor_filtered = bit.rshift(cam_stat_monitor, 4)
 			if cam_monitor_filtered == 0x00 then
 				cam_monitor_string = "Non disponibile"
 			elseif cam_monitor_filtered == 0x01 then
@@ -151,7 +188,7 @@ function svs_protocol.dissector(buffer, pinfo, tree)
 				cam_monitor_string = "Errore"
 			end
 
-			cam_roi_filtered = bit.band(second, 0x0F)
+			cam_roi_filtered = bit.band(cam_roi_polarity, 0x0F)
 			if cam_roi_filtered == 0x00 then
 				cam_roi_string = "Non disponibile"
 			elseif cam_roi_filtered == 0x01 then
@@ -164,7 +201,7 @@ function svs_protocol.dissector(buffer, pinfo, tree)
 				cam_roi_string = "Top Half"
 			end
 
-			cam_polarity_filtered = bit.rshift(second, 4)
+			cam_polarity_filtered = bit.rshift(cam_roi_polarity, 4)
 			if cam_polarity_filtered == 0x00 then
 				cam_polarity_string = "Non disponibile"
 			elseif cam_polarity_filtered == 0x01 then
@@ -177,10 +214,10 @@ function svs_protocol.dissector(buffer, pinfo, tree)
 				cam_polarity_string = "NUC Automatico Abilitato"
 			end
 
-			minitree:add(camera_status, cam_stat_filtered):append_text(" (" .. cam_stat_str .. ")")
-			minitree:add(camera_monitor, cam_monitor_filtered):append_text(" (" .. cam_monitor_string .. ")")
-			minitree:add(camera_ROI, cam_roi_filtered):append_text(" (" .. cam_roi_string .. ")")
-			minitree:add(camera_Polarity, bit.rshift(second, 4)):append_text(" (" .. cam_polarity_string .. ")")
+			minitree:add(camera_status, buffer(i,1)):append_text(" (" .. cam_stat_str .. ")")
+			minitree:add(camera_monitor, buffer(i,1)):append_text(" (" .. cam_monitor_string .. ")")
+			minitree:add(camera_ROI, buffer(i+1,1)):append_text(" (" .. cam_roi_string .. ")")
+			minitree:add(camera_Polarity, buffer(i+1,1)):append_text(" (" .. cam_polarity_string .. ")")
 
 			index = index + 1
 		end
@@ -196,7 +233,7 @@ function svs_protocol.dissector(buffer, pinfo, tree)
 				extended_string = "Error"
 		end
 
-		subtree:add(extended_type, extended_filtered):append_text(" (" .. extended_string .. ")")
+		subtree:add(extended_type, buffer(64,1)):append_text(" (" .. extended_string .. ")")
 		subtree:add(checksum_type, buffer(65,1)):append_text("")
 		subtree:add(flagstop_type, buffer(66,1)):append_text("")
 	end
